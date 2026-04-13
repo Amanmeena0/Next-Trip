0a5eda562402edf6abba8ac0c3636575c8effeec
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-13 15:19:04 +0530

    message

--- Server/Agents/flight_agent/__main__.py
+++ Server/Agents/flight_agent/__main__.py
@@ -3,4 +3,4 @@ from Agents.flight_agent.task_manager import run
 app = create_app(agent=type("Agent", (), {"execute": run}))
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, port=8004)
+    uvicorn.run(app, port=8001)
