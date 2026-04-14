e4d737554c766f9a97330f9ec1054a45ba07dc7d
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-14 22:41:34 +0530

    Add Hugging Face model integration and update environment variable handling

--- Server/Model/Model.py
+++ Server/Model/Model.py
@@ -1,11 +1,15 @@
 import os
 from dotenv import load_dotenv, find_dotenv
+from google.adk.models.lite_llm import LiteLlm
 
 load_dotenv(find_dotenv())
 
 MODEL_NAME = os.getenv("MODEL_NAME", "gemini/gemini-2.5-flash")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
+
+HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
+
 
 if not GEMINI_API_KEY:
     raise ValueError("GEMINI_API_KEY is missing")
@@ -18,3 +22,6 @@ def get_api_key():
 
 def get_open_model():
     return OPENAI_API_KEY
+
+def get_hugging_face():
+    return LiteLlm(model="huggingface/meta-llama/Meta-Llama-3-8B-Instruct")
