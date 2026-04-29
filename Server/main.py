b4e0d898fb76820b78856281609d15411e011dec
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-29 22:26:46 +0530

    added a clas in the main.py to define the variables

--- Server/main.py
+++ Server/main.py
@@ -3,11 +3,20 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
+from datetime import date
 
 class Trip(BaseModel):
     CurrentCity: str
     DestinationCity : str
-    FromDate : 
-    ToDate :
-    Budget :
-    
+    FromDate : date
+    ToDate : date
+    Budget : float
+ 
+app = FastAPI(debug=True)
+
+origins = [
+    "https://localhost:3000"
+]
+
+
+
